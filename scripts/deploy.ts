fe6ef7bc2ffbe4c5c19dbf5d58222e8f8d4adc8e
Hynn <choihwoong@me.com>
2023-06-19 16:40:20 +0900

    feat:testcode(tdd)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [owner] = await ethers.getSigners()
-  
-  const tokenFactory = await ethers.getContractFactory("MyToken");
-  const myToken = await tokenFactory.deploy();
-  
-  await myToken.deployed();
-  console.log(`The Token is :${await myToken.name()} & ${await myToken.symbol()}Token deployed to: ${myToken.address}`);
-  console.log(`The Token Minter ${await myToken.minter()}`);
+  const TraderFactory = await ethers.getContractFactory("Trader");
+  const trader = await TraderFactory.deploy();
 
+  await trader.deployed();
+
+  console.log(`Trader deployed: ${trader.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
